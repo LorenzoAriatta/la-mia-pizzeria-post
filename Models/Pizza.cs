cf764b0aef596f01479742a3476c7e750bc8ac19
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-18 15:44:10 +0200

    Add validate attributes

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -6,16 +6,30 @@ public class Pizza
 {
     [Key]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Il campo è obbligatorio")]
+    [StringLength(40, ErrorMessage = "Il nome non può avere più di 40 caratteri")]
     public string Name { get; set; }
 
     [Column (TypeName = "text")]
+    [Required(ErrorMessage = "Il campo è obbligatorio")]
+    [StringLength(255, ErrorMessage = "La descrizioe non può avere più di 255 caratteri")]
     public string Description { get; set; }
 
+    [Required(ErrorMessage = "Il Campo è obbligatorio")]
+    [Url(ErrorMessage = "Il link non è valido")]
     public string Image { get; set; }
 
+    [Required(ErrorMessage = "Il campo è obbligatiorio")]
+    [Range(1d, 15d, ErrorMessage = "Il prezzo deve essere compreso tra 1 e 15 &euro;")]
     public double Price { get; set; }
 
-    public List<Ingrediente> ingredients { get; set; }
+    public List<Ingrediente>? ingredients { get; set; }
+
+    public Pizza()
+    {
+
+    }
 
     public Pizza(string name, string description, string image, double price, int id)
     {
